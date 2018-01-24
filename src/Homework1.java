f9b2462a48a122d4e0323719f862b317ce570fa7
pathakorn <baybede.i5@gmail.com>
2018-01-24 21:45:37 +0700

    590610638-2

--- src/Homework1.java
+++ src/Homework1.java
@@ -2,10 +2,20 @@
 package Homework1;
 
 import java.util.Stack;
+import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellRenderer;
+import javax.swing.tree.TreeSelectionModel;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import java.net.URL;
+import java.io.IOException;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.util.Stack;
 
 
-
-public class Homework1 {
+/*public class Homework1 {
 
 	public static String inPut;
 
@@ -32,7 +42,107 @@ public class Homework1 {
 		tree = (node) a.pop();
 		return tree;
 
-	}
+	}*/
+
+    public class Homework1 extends JPanel
+      implements TreeSelectionListener {
+            private JEditorPane htmlPane;
+            private JTree tree;
+            private static Node root;
+            private static String get;
+           private static boolean useleaf = false;
+
+        private static boolean useSystemLookAndFeel = false;
+
+            public Homework1() {
+                super(new GridLayout(1,0));
+                            //Create the nodes.
+                DefaultMutableTreeNode top = new DefaultMutableTreeNode(root);
+                    CreateNodes(top,root);
+
+                    //Create a tree that allows one selection at a time.
+                     tree = new JTree(top);
+                    tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+
+                            //Listen for when the selection changes.
+                tree.addTreeSelectionListener(this);
+
+                           if (playWithLineStyle) {
+                            System.out.println("line style = " + lineStyle);
+                           tree.putClientProperty("JTree.lineStyle", lineStyle);
+                        }
+
+                            //Create the scroll pane and add the tree to it.
+                                    JScrollPane treeView = new JScrollPane(tree);
+
+                            //Create the HTML viewing pane.
+                                    htmlPane = new JEditorPane();
+                    htmlPane.setEditable(false);
+                    JScrollPane htmlView = new JScrollPane(htmlPane);
+
+                            //Add the scroll panes to a split pane.
+                                   JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+                    splitPane.setTopComponent(treeView);
+                    splitPane.setBottomComponent(htmlView);
+
+                            Dimension minimumSize = new Dimension(100, 50);
+                    htmlView.setMinimumSize(minimumSize);
+                    treeView.setMinimumSize(minimumSize);
+                    splitPane.setDividerLocation(100);
+                    splitPane.setPreferredSize(new Dimension(500, 300));
+
+                           //Add the split pane to this panel.
+                add(splitPane);
+
+                           ImageIcon leafIcon = createImageIcon("middle.gif");
+                    if (leafIcon != null) {
+                            DefaultTreeCellRenderer renderer =
+                                           new DefaultTreeCellRenderer();
+                           renderer.setClosedIcon(leafIcon);
+                            renderer.setOpenIcon(leafIcon);
+                            tree.setCellRenderer(renderer);
+                        }
+               }
+    protected static ImageIcon createImageIcon(String path) {
+                   java.net.URL imgURL = Homework1.class.getResource(path);
+                    if (imgURL != null) {
+                            return new ImageIcon(imgURL);
+                        } else {
+                            System.err.println("Couldn't find file: " + path);
+                            return null;
+                        }
+               }
+   /** Required by TreeSelectionListener interface. */
+            public void valueChanged(TreeSelectionEvent e) {
+                    DefaultMutableTreeNode node = (DefaultMutableTreeNode)
+                                   tree.getLastSelectedPathComponent();
+
+                           if (node == null) return;
+                    useleaf=node.isLeaf();
+                    Object nodeInfo = node.getUserObject();
+                   DisplayNode((Node)nodeInfo);
+
+                        }
+
+
+            public static String number;
+
+            public static void main(String[] args) {
+
+
+                                    number = args[0];
+            //        number = "251-*32*+";
+                            char[] cha = number.toCharArray();
+
+                             root = Stack(cha);
+                     get = infix(root);
+                    javax.swing.SwingUtilities.invokeLater(new Runnable() {
+           public void run() {
+               createAndShowGUI(); }
+             });
+
+            }
+
 
 	public static void inorder(node n){
 		if(n==null){
@@ -124,6 +234,15 @@ public class Homework1 {
 		return summ;
 	}
 
+    public static boolean is_number(char letter) {
+
+                if (letter == '1' || letter == '2' || letter == '3' || letter == '4' || letter == '5' || letter == '6' || letter == '7' || letter == '8' || letter == '9' || letter == '0') {
+
+                    return true;
+                   }
+                return false;
+                    }
+
 	public static void main(String[] args) {
 
 		String in ="251-*32*+";
@@ -136,3 +255,56 @@ public class Homework1 {
 		System.out.print("=" + ans);
 	}
 }
+
+	private static void createAndShowGUI() {
+		        if (useSystemLookAndFeel) {
+			           try {
+				               UIManager.setLookAndFeel(
+		                       UIManager.getSystemLookAndFeelClassName());
+				            } catch (Exception e) {
+				                System.err.println("Couldn't use system look and feel.");
+				           }
+			       }
+
+				       //Create and set up the window.
+						       JFrame frame = new JFrame("Jtree");
+		        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+				        //Add content to the window.
+						        frame.add(new Homework1());
+
+				        //Display the window.
+						        frame.pack();
+		        frame.setVisible(true);
+		    }
+
+
+
+
+   public static Node Stack(char cha[]) {
+
+                Stack<Node> data = new Stack();
+               Node node, num1, num2;
+
+                for (int i = 0; i < cha.length; i++) {
+
+                    if (!is_operater(cha[i])) {
+
+                        node = new Node(cha[i]);
+                        data.push(node);
+
+                    } else {
+
+                        node = new Node(cha[i]);
+                        num2 = data.pop();
+                        num1 = data.pop();
+                        node.left = num1;
+                        node.right = num2;
+
+                        data.push(node);
+                    }
+                }
+                node = data.peek();
+                return node;
+            }
+
